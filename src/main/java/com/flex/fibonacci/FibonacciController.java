b0681acf3051f6abd2420a6204f6a57d5854c808
ColeHassett <colehassett@gmail.com>
2024-03-24 13:42:21 -0400

    add endpoint functionality

--- src/main/java/com/flex/fibonacci/FibonacciController.java
+++ src/main/java/com/flex/fibonacci/FibonacciController.java
@@ -1,24 +1,41 @@
 package com.flex.fibonacci;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class FibonacciController {
 
+    public Integer index = 0;
+    public Double value = 0.0;
+
+    // Golden value (phi)
+    public static final Double golden = (1 + Math.sqrt(5)) / 2;
+
     @GetMapping("/current")
-    public Integer current() {
-        
-        return null;
-    }
+    public Double current() { return value; }
 
     @GetMapping("/next")
-    public Integer next() {
-        
-        return null;
+    public Double next() {
+        index += 1;
+        return calcValue();
     }
 
     @GetMapping("/previous")
-    public Integer previous() {
-        
-        return null;
+    public Double previous() {
+        if (index > 0) {
+            index -= 1;
+        }
+        return calcValue();
+    }
+
+    /**
+     * Calculate and return the Fibonacci value at the current index
+     * 
+     * @return Fibonacci sequence value at current index
+     */
+    private Double calcValue() {
+        value = Math.rint((Math.pow(golden, index) - Math.pow((1-golden), index)) / Math.sqrt(5));
+        return value;
     }
 }
